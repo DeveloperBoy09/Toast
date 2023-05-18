8a7d905cf9a9c6f0bb08587999fe27990939e15c
Developer_Deepak <deepak63022kr@gmail.com>
2023-05-18 15:28:48 +0530

    commit

---
+++ app/src/main/java/com/example/tostintentactivity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tostintentactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            Toast(this).showCustomToast("Login Successful",this)
+        }
+    }
+}
